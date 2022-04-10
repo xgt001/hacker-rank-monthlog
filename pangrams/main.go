7623ca6f25ecad6cb031366caa65c2742bca06e0
Ganesh Hegde <ganesh.hegde@mailbox.org>
2022-04-10 13:33:02 +0200

    add 2 more

---
+++ pangrams/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+func pangrams(s string) string {
+	status := "pangram"
+	fail := "not pangram"
+	// Write your code here
+	lowString := strings.ToLower(s)
+	for ch := 'a'; ch <= 'z'; ch++ {
+		if !strings.Contains(lowString, string(ch)) {
+			status = fail
+		}
+	}
+	return status
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	s := readLine(reader)
+
+	result := pangrams(s)
+
+	fmt.Fprintf(writer, "%s\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
